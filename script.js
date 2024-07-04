5bedea1e3e902ffb92003fd3fe1e4e443d2dd0e1
Saurabh Singh Rautela <saurabhrautela786@gmail.com>
2024-07-04 11:55:54 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,160 @@
+
+tl=gsap.timeline()
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  tl.from(".menu-links",{
+      scale:0,
+      duration:0.1,
+
+  })
 }
+
+function animate(){
+  // page 1
+  tl.from(".logo",{
+    y:100,
+    duration:1,
+    opacity:0,
+    stagger:0.1
+  })
+  tl.from(".list li",{
+    y:100,
+    duration:1,
+    opacity:0,
+    stagger:0.05,
+  })
+  tl.from("#profile .section__pic-container",{
+    x:-200,
+    opacity:0,
+    scale:0,
+    rotate:45,
+    duration:1,
+    stagger:0.1
+  })
+  tl.from(".section__text .bio",{
+    x:100,
+    opacity:0,
+    duration:1,
+    stagger:0.2
+  })
+  tl.from(".section__text .btn-container",{
+    opacity:0,
+    duration:0.2,
+    stagger:0.1
+  })
+  tl.from("#socials-container img",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    stagger:0.1
+  })
+
+  //page 2
+
+  tl.from(".about-section .ab,.ab2,.about-section div",{
+    scale:0,
+    duration:2,
+    opacity: 0,
+    stagger:0.2,
+    scrollTrigger:{
+      trigger:".about-section .para",
+      scroller:"body",
+      end:"top 70%",
+      scrub:5
+    }
+  })
+  tl.from(".experience-sec .ex1,.experience-sec .ex2,.experience-sec div",{
+    scale:0,
+    duration:2,
+    opacity: 0,
+    stagger:0.7,
+    
+    scrollTrigger:{
+      trigger:".experience-sec .details-container",
+      scroller:"body",
+      end:"top 70%",
+      scrub:5
+    }
+  })
+
+
+
+  //project animation
+  tl.from(".project-section .pro1,.project-section .pro2",{
+    scale:0,
+    duration:1,
+    opacity: 0,
+    stagger:0.2,
+    
+    scrollTrigger:{
+      trigger:".project-section .pro1",
+      scroller:"body",
+      scrub:5
+    }
+  })
+  tl.from(".project-section .pro-1",{
+    x:-300,
+    duration:2,
+    opacity: 0,
+    stagger:0.7,
+    
+    scrollTrigger:{
+      trigger:".project-section .pro1",
+      scroller:"body",
+      scrub:5
+    }
+  })
+  tl.from(".project-section .pro-2",{
+    x:300,
+    duration:2,
+    opacity: 0,
+    stagger:0.7,
+    
+    scrollTrigger:{
+      trigger:".project-section .pro1",
+      scroller:"body",
+      scrub:5
+    }
+  })
+  // tl.from(".project-section .pro-3",{
+  //   scale:0,
+  //   delay:1,
+  //   duration:2,
+  //   opacity: 0,
+  //   stagger:0.7,
+    
+  //   scrollTrigger:{
+  //     trigger:".about-3",
+  //     scroller:"body",
+  //     end:"top 20%",
+  //     scrub:5
+  //   }
+  // })
+
+
+
+
+  //contact
+
+  tl.from(".contact .con1,.contact .con1,.contact div",{
+    scale:0,
+    duration:2,
+    opacity: 0,
+    stagger:0.7,
+    
+    scrollTrigger:{
+      trigger:".contact .contact-info-upper-container",
+      scroller:"body",
+      end:"top 70%",
+      scrub:5
+    }
+  })
+
+
+}
+animate();
+
+
