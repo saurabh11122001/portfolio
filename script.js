a304104769040b7b4d5f651dfaa2b43d5ea89499
Saurabh Singh Rautela <saurabhrautela786@gmail.com>
2024-09-08 19:56:29 +0530

    updated animation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// locomotive setup
 tl=gsap.timeline()
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
@@ -8,174 +7,36 @@ function toggleMenu() {
   tl.from(".menu-links",{
       scale:0,
       duration:0.1,
-
-  })
+  });
 }
 
 window.onload = function() {
   gsap.registerPlugin(ScrollTrigger);
-  // page 1
   ScrollTrigger.matchMedia({
-    // Large and medium screens
     "(min-width: 768px)": function() {
-      tl.from(".logo",{
-        y:100,
-        duration:1,
-        opacity:0,
-        stagger:0.1
-      })
-      tl.from(".list li",{
-        y:100,
-        duration:1,
-        opacity:0,
-        stagger:0.05,
-      })
-      tl.from("#profile .section__pic-container",{
-        x:-200,
-        opacity:0,
-        scale:0,
-        rotate:45,
-        duration:1,
-        stagger:0.1
-      })
-      tl.from(".section__text .bio",{
-        x:100,
-        opacity:0,
-        duration:1,
-        stagger:0.2
-      })
-      tl.from(".section__text .btn-container",{
-        opacity:0,
-        duration:0.2,
-        stagger:0.1
-      })
-      tl.from("#socials-container img",{
-        scale:0,
-        opacity:0,
-        duration:1,
-        stagger:0.1
-      })
-    
-      //page 2
-    
-      tl.from(".about-section .ab,.ab2,.about-section div",{
-        scale:0,
-        duration:2,
-        opacity: 0,
-        stagger:0.2,
-        scrollTrigger:{
-          trigger:".about-section .ab",
-          scroller:"body",
-          start:"top 90%",
-          scrub:5
-        }
-      })
-      tl.from(".experience-sec .ex1,.experience-sec .ex2,.experience-sec div",{
-        scale:0,
-        duration:2,
-        opacity: 0,
-        stagger:0.7,
-        
-        scrollTrigger:{
-          trigger:".experience-sec .details-container",
-          scroller:"body",
-          end:"top 70%",
-          scrub:5
-        }
-      })
-    
-    
-    
-      //project animation
-      tl.from(".project-section .pro1,.project-section .pro2",{
-        scale:0,
-        duration:1,
-        opacity: 0,
-        stagger:0.2,
-        
-        scrollTrigger:{
-          trigger:".project-section .pro1",
-          scroller:"body",
-          scrub:5
-        }
-      })
-      tl.from(".pro-1",{
-        x:-300,
-        duration:2,
-        opacity: 0,
-        stagger:0.7,
-        
-        scrollTrigger:{
-          trigger:".project-section .pro1",
-          scroller:"body",
-          scrub:5
-        }
-      })
-      tl.from(".pro-2",{
-        x:300,
-        duration:2,
-        opacity: 0,
-        stagger:0.7,
-        
-        scrollTrigger:{
-          trigger:".project-section .pro3",
-          scroller:"body",
-          scrub:5
-        }
-      })
-      tl.from(".pro-3",{
-        x:-200,
-        opacity: 0,
-        stagger:0.7,
-        
-        scrollTrigger:{
-          trigger:".pro-3",
-          scroller:"body",
-          scrub:5,
-          end:"top 20%"
-        }
-      })
-      tl.from(".pro-4",{
-        x:200,
-        duration:2,
-        opacity: 0,
-        stagger:0.7,
-        
-        scrollTrigger:{
-          trigger:".pro-3",
-          scroller:"body",
-          scrub:5
-        }
-      })
-    
-      tl.from(".contact .con1,.contact .con1,.contact div",{
-        scale:0,
-        duration:2,
-        opacity: 0,
-        stagger:0.7,
-        
-        scrollTrigger:{
-          trigger:".contact .contact-info-upper-container",
-          scroller:"body",
-          end:"top 70%",
-          scrub:5
-        }
-      })
-    
-    
-    
-    
-    
+      tl.fromTo(".logo", {visibility: "visible", y: 100, opacity: 0}, {y: 0, opacity: 1, duration: 1, stagger: 0.1});
+      tl.fromTo(".list li", {visibility: "visible", y: 100, opacity: 0}, {y: 0, opacity: 1, duration: 1, stagger: 0.05});
+      tl.fromTo("#profile .section__pic-container", {visibility: "visible", x: -200, opacity: 0, scale: 0, rotate: 45}, {x: 0, opacity: 1, scale: 1, rotate: 0, duration: 1, stagger: 0.1});
+      tl.fromTo(".section__text .bio", {visibility: "visible", x: 100, opacity: 0}, {x: 0, opacity: 1, duration: 1, stagger: 0.2});
+      tl.fromTo(".section__text .btn-container", {visibility: "visible", opacity: 0}, {opacity: 1, duration: 0.2, stagger: 0.1});
+      tl.fromTo("#socials-container img", {visibility: "visible", scale: 0, opacity: 0}, {scale: 1, opacity: 1, duration: 1, stagger: 0.1});
 
+      // For other animations
+      tl.fromTo(".about-section .ab,.ab2,.about-section div", {visibility: "visible", scale: 0, opacity: 0}, {scale: 1, opacity: 1, duration: 2, stagger: 0.2, scrollTrigger:{trigger:".about-section .ab", scroller:"body", start:"top 90%", scrub: 5}});
+      tl.fromTo(".experience-sec .ex1,.experience-sec .ex2,.experience-sec div", {visibility: "visible", scale: 0, opacity: 0}, {scale: 1, opacity: 1, duration: 2, stagger: 0.7, scrollTrigger:{trigger:".experience-sec .details-container", scroller:"body", end:"top 70%", scrub: 5}});
+      tl.fromTo(".project-section .pro1,.project-section .pro2", {visibility: "visible", scale: 0, opacity: 0}, {scale: 1, opacity: 1, duration: 1, stagger: 0.2, scrollTrigger:{trigger:".project-section .pro1", scroller:"body", scrub: 5}});
+      tl.fromTo(".pro-1", {visibility: "visible", x: -300, opacity: 0}, {x: 0, opacity: 1, duration: 2, stagger: 0.7, scrollTrigger:{trigger:".project-section .pro1", scroller:"body", scrub: 5}});
+      tl.fromTo(".pro-2", {visibility: "visible", x: 300, opacity: 0}, {x: 0, opacity: 1, duration: 2, stagger: 0.7, scrollTrigger:{trigger:".project-section .pro3", scroller:"body", scrub: 5}});
+      tl.fromTo(".pro-3", {visibility: "visible", x: -200, opacity: 0}, {x: 0, opacity: 1, duration: 2, stagger: 0.7, scrollTrigger:{trigger:".pro-3", scroller:"body", scrub: 5, end: "top 20%"}})
+      tl.fromTo(".pro-4", {visibility: "visible", x: 200, opacity: 0}, {x: 0, opacity: 1, duration: 2, stagger: 0.7, scrollTrigger:{trigger:".pro-3", scroller:"body", scrub: 5}});
+      tl.fromTo(".contact .con1,.contact div", {visibility: "visible", scale: 0, opacity: 0}, {scale: 1, opacity: 1, duration: 2, stagger: 0.7, scrollTrigger:{trigger:".contact .contact-info-upper-container", scroller:"body", end: "top 70%", scrub: 5}});
     }
-  })
-      
-}
-let resizeTimer;
+  });
+};
+
 window.addEventListener('resize', function() {
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(function() {
-    // Reinitialize animations
     ScrollTrigger.refresh();
   }, 250);
 });
